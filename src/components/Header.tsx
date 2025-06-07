c16579d38b5bc7590d3a602642ba61a72dce14ca
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 11:50:59 -0700

    Added function skeleton for Header component.
    - added <header> html

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,4 +3,9 @@ import { Container } from '@/components/Container'
 import { DiamondIcon } from '@/components/DiamondIcon'
 import { Logo } from '@/components/Logo'
 
-
+export function Header() {
+  return (
+    <header className="relative z-50 flex-none lg:pt-11">
+    </header>
+  )
+}
