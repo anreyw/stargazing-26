8e543796452d7a9dfd9307db95bbbfbcb27d9c35
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 16:47:53 -0700

    Removed the test button from main page.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,9 +2,5 @@ import { BackgroundImage } from '@/components/BackgroundImage'
 import { Button } from '@/components/Button'
 
 export default function Page() {
-  return (
-    <Button href="/" className="m-32" >
-      Register
-    </Button>
-  )
+  return <></>
 }
