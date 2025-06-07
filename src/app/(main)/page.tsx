8c2335e2276930c07dfaba4c453f570a2a367a12
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 10:45:31 -0700

    Added Test Button to Main Page.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,10 @@
 import { BackgroundImage } from '@/components/BackgroundImage'
+import { Button } from '@/components/Button'
 
 export default function Page() {
-  return <BackgroundImage />
+  return (
+    <Button href="/" className="m-32" >
+      Register
+    </Button>
+  )
 }
