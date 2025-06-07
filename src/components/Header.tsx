0ad97641650c940e76c62f0c339ead2e6e7c0889
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 11:55:43 -0700

    Added Container component into the Header component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,8 @@ import { Logo } from '@/components/Logo'
 export function Header() {
   return (
     <header className="relative z-50 flex-none lg:pt-11">
+      <Container className="">
+      </Container>
     </header>
   )
 }
