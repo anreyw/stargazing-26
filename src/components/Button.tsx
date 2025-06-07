3830decf16ee74bf858631d637dc6ae00a7ec2e8
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 10:10:34 -0700

    Further built Button prop.
    - added type definition for the Button Props
    - created the Button function with skeletal CSS
    - added return statement with ternary

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,19 @@
 import Link from 'next/link'
 import clsx from 'clsx'
 
+type ButtonProps =
+  | React.ComponentPropsWithoutRef<typeof Link>
+  | (React.ComponentPropsWithoutRef<'button'> & { href?: undefined })
+
+export function Button({ className, ...props }: ButtonProps) {
+  className = clsx(
+    'inline-flex',
+    className,
+  )
+
+  return typeof props.href === 'undefined' ? (
+    <button className={className} {...props} />
+  ) : (
+    <Link className={className} {...props} />
+  )
+}
