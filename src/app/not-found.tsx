d99014399f1659364d09c3dd32239fa1a3ec8a06
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 20:07:51 -0700

    Added the BackgroundImage component with positioning.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,6 +7,7 @@ export default function NotFound() {
   return (
     <Layout showFooter={false}>
       <div className="relative flex h-full items-center py-20 sm:py-36">
+	<BackgroundImage className="-top-36 bottom-0" />
       </div>
     </Layout>
   )
