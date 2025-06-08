9ba9f084efe2c7e23d5b4c706f7a56c32951a51c
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-08 10:24:46 -0700

    Import clsx function into Speakers.

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -3,3 +3,4 @@
 import { useEffect, useId, useState } from 'react'
 import Image from 'next/image'
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import clsx from 'clsx'
