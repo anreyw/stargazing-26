365eae532b97dda8a7ef46be816e5ab5cb6c9de7
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 21:51:26 -0700

    Added dl of sample conference details.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,6 +26,19 @@ export function Hero() {
 	  <Button href="#" className="mt-10 w-full sm:hidden">
 	    Register
 	  </Button>
+	  <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
+	    {[
+	      ['Speakers', '18'],
+              ['People Attending', '2,091'],
+              ['Venue', 'Staples Center'],
+              ['Location', 'Los Angeles'],
+	    ].map(([name, value]) => (
+	      <div key={name}>
+		<dt className="font-mono text-sm text-blue-600">{name}</dt>
+		<dd className="mt-0.5 text-2xl font-semibold tracking-tight text-blue-900">{value}</dd>
+	      </div>
+	    ))}
+	  </dl>
 	</div>
       </Container>
     </div>
