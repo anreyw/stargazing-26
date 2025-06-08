1a6730a405aac76c33561bf2a4e6f7ed60f72155
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-08 10:37:37 -0700

    Copied the `days` array with sample speaker data into Speakers.

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -27,3 +27,116 @@ import ronniCantadoreImage from '@/images/avatars/ronni-cantadore.jpg'
 import stevenMchailImage from '@/images/avatars/steven-mchail.jpg'
 import waylonHydenImage from '@/images/avatars/waylon-hyden.jpg'
 
+const days = [
+  {
+    name: 'Opening Day',
+    date: 'April 4',
+    dateTime: '2022-04-04',
+    speakers: [
+      {
+        name: 'Steven McHail',
+        role: 'Designer at Globex Corporation',
+        image: stevenMchailImage,
+      },
+      {
+        name: 'Jaquelin Isch',
+        role: 'UX Design at InGen',
+        image: jaquelinIschImage,
+      },
+      {
+        name: 'Dianne Guilianelli',
+        role: 'General Manager at Initech',
+        image: dianneGuilianelliImage,
+      },
+      {
+        name: 'Ronni Cantadore',
+        role: 'Design Engineer at Weyland-Yutani',
+        image: ronniCantadoreImage,
+      },
+      {
+        name: 'Erhart Cockrin',
+        role: 'Product Lead at Cyberdyne Systems',
+        image: erhartCockrinImage,
+      },
+      {
+        name: 'Parker Johnson',
+        role: 'UI Designer at MomCorp',
+        image: parkerJohnsonImage,
+      },
+    ],
+  },
+  {
+    name: 'Speakers & Workshops',
+    date: 'April 5',
+    dateTime: '2022-04-05',
+    speakers: [
+      {
+        name: 'Damaris Kimura',
+        role: 'Senior Engineer at OCP',
+        image: damarisKimuraImage,
+      },
+      {
+        name: 'Ibrahim Frasch',
+        role: 'Programmer at Umbrella Corp',
+        image: ibrahimFraschImage,
+      },
+      {
+        name: 'Cathlene Burrage',
+        role: 'Frontend Developer at Buy n Large',
+        image: cathleneBurrageImage,
+      },
+      {
+        name: 'Rinaldo Beynon',
+        role: 'Data Scientist at Rekall',
+        image: rinaldoBeynonImage,
+      },
+      {
+        name: 'Waylon Hyden',
+        role: 'DevOps at RDA Corporation',
+        image: waylonHydenImage,
+      },
+      {
+        name: 'Giordano Sagucio',
+        role: 'Game Developer at Soylent Corp',
+        image: giordanoSagucioImage,
+      },
+    ],
+  },
+  {
+    name: 'Interviews',
+    date: 'April 6',
+    dateTime: '2022-04-06',
+    speakers: [
+      {
+        name: 'Andrew Greene',
+        role: 'Frontend Developer at Ultratech',
+        image: andrewGreeneImage,
+      },
+      {
+        name: 'Heather Terry',
+        role: 'Backend Developer at Xanatos Enterprises',
+        image: heatherTerryImage,
+      },
+      {
+        name: 'Piers Wilkins',
+        role: 'Full stack Developer at BiffCo',
+        image: piersWilkinsImage,
+      },
+      {
+        name: 'Gordon Sanderson',
+        role: 'Mobile Developer at Cobra Industries',
+        image: gordonSandersonImage,
+      },
+      {
+        name: 'Kimberly Parsons',
+        role: 'Game Developer at Tyrell Corporation',
+        image: kimberlyParsonsImage,
+      },
+      {
+        name: 'Richard Astley',
+        role: 'CEO at Roll Out',
+        image: richardAstley,
+      },
+    ],
+  },
+]
