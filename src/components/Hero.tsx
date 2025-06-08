c897f67556ccab82368186be983203fd1c2ca916
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 21:14:53 -0700

    Added Hero h1 with parent layout div.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,11 @@ export function Hero() {
     <div className="relative py-20 sm:pt-36 sm:pb-24">
       <BackgroundImage className="-top-36 -bottom-14" />
       <Container className="relative">
-	
+	<div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
+	  <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
+	    <span className="sr-only">DeceptiConf - </span>A design conference for the dark side.
+	  </h1>
+	</div>
       </Container>
     </div>
   )
