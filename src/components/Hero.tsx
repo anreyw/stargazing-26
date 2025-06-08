914857a2113a12371af55aebd060a3998488acc5
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 21:08:03 -0700

    Added BackrgoundImage to Hero.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,11 @@
 import { BackgroundImage } from '@/components/BackgroundImage'
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
+
+export function Hero() {
+  return (
+    <div>
+      <BackgroundImage className="" />
+    </div>
+  )
+}
