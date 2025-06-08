bc08aeed3cf7eb26584d2e075c8e5827b70df2e1
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 20:02:30 -0700

    Added styling div to not-found page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,6 +6,8 @@ import { Layout } from '@/components/Layout'
 export default function NotFound() {
   return (
     <Layout showFooter={false}>
+      <div className="relative flex h-full items-center py-20 sm:py-36">
+      </div>
     </Layout>
   )
 }
