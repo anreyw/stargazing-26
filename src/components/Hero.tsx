7bbf60ebed3cbf0f5327444e21e42fd8049f930a
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 21:10:45 -0700

    Added Container to Hero. Add styling to parent div.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,11 @@ import { Container } from '@/components/Container'
 
 export function Hero() {
   return (
-    <div>
-      <BackgroundImage className="" />
+    <div className="relative py-20 sm:pt-36 sm:pb-24">
+      <BackgroundImage className="-top-36 -bottom-14" />
+      <Container className="relative">
+	
+      </Container>
     </div>
   )
 }
