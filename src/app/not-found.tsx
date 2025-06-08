bf3af81c78d963260a135cb5dbe79cd72621a60e
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-07 17:07:20 -0700

    Started building the nextjs not-found page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from '@/components/BackgroundImage'
+import { Button } from '@/components/Button'
+import { Container } from '@/components/Container'
+import { Layout } from '@/components/Layout'
+
+export default function NotFound() {
+  return (
+    <Layout showFooter={false}>
+    </Layout>
+  )
+}
