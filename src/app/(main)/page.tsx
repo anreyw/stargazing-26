9ea50dabbbd9451e5c94d9c708e97a28392ba77d
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-08 10:42:02 -0700

    Added Speakers component to Home Page.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/Hero'
+import { Speakers } from '@/components/Speakers'
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Speakers />
     </>
   )
 }
