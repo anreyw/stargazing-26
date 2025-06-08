914728a127ee929e81cde4abb83b8c9290eb571e
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-08 10:32:41 -0700

    Imported Container, DimaondIcon into Speakers.

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -4,3 +4,6 @@ import { useEffect, useId, useState } from 'react'
 import Image from 'next/image'
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import { DiamondIcon } from '@/components/DiamondIcon'
