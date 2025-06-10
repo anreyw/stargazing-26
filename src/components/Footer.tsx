4beeace8747cd5c91f52aaddf241f1350bf23021
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-10 09:09:46 -0700

    chore: scaffold Footer component structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,2 +1,10 @@
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
+
+export function Footer() {
+  return (
+    <footer>
+      Footer
+    </footer>
+  )
+}
