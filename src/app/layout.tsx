5a652f3dd0a50b2f642ef57ba744a0282f88c829
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-10 16:45:58 -0700

    Remove installed fonts.
    Will probably switch back to using Geist, but in any case, start
    project back with no font installation.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,7 @@
 import { type Metadata } from 'next'
-import { DM_Sans, Inter } from 'next/font/google'
 import { clsx } from 'clsx'
 
 import '@/styles/globals.css'
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
-
-const dmSans = DM_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '700'],
-  display: 'swap',
-  variable: '--font-dm-sans',
-})
 
 export const metadata: Metadata = {
   title: {
@@ -31,13 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en"
-      className={clsx(
-	'h-full bg-white antialiased',
-	inter.variable,
-	dmSans.variable,
-      )
-    }>
+    <html lang="en" className="h-full bg-white">
       <body className="flex min-h-full">
 	<div className="flex w-full flex-col"> 
 	  {children}
