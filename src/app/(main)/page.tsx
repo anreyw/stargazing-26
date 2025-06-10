df0e8ea2d850af35421d4b37bfe378305bd86012
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-09 17:03:29 -0700

    Added Newsletter to the Main Homepage.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/components/Hero'
 import { Speakers } from '@/components/Speakers'
+import { Newsletter } from '@/components/Newsletter'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Speakers />
+      <Newsletter />
     </>
   )
 }
