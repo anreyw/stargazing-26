4e73544764d664cc3f0de23ccdc80c69d50ed351
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-10 09:33:20 -0700

    Add copyright statement to Footer, with styling.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ export function Footer() {
     <footer className="flex-none py-16">
       <Container className="flex flex-col items-center justify-between md:flex-row">
 	<Logo className="h-12 w-auto" />
-	<p>Footer</p>
+	<p className="mt-6 text-base text-slate-500 md:mt-0">
+	  Copyright &copy; {new Date().getFullYear()} DeceptiConf, LLC. All
+          rights reserved.
+	</p>
       </Container>
     </footer>
   )
