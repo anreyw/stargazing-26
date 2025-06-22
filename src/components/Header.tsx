7d76dc4b2e7c6ee0b7ced83d4eadc8bbb6edf53e
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-22 16:28:08 +0800

    Add CSS to make Header sticky.
    Inspired by the React.dev header, add Tailwind CSS to the header
    element to make it sticky with other sublte styling.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white">
+    <header className="sticky top-0 z-50 bg-white/75 backdrop-blur shadow transition duration-300">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
