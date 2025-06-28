35d2d35358bb08b13a8ac322f5c85ba4e61d3ea7
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-28 09:27:41 -0700

    Change name of one nav link
    In order to test understanding of the code, change the name of
    one of the nav link, Features, to Events.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -107,7 +107,7 @@ export default function Example() {
           </Popover>
 
           <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Features
+            Events
           </a>
           <a href="#" className="text-sm/6 font-semibold text-gray-900">
             Marketplace
@@ -168,7 +168,7 @@ export default function Example() {
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Features
+                  Events
                 </a>
                 <a
                   href="#"
