962cc5ce6576eb335740d343aa4495ac50f93adc
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-29 17:30:09 -0700

    Import components from HeadlessUI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,17 @@
 'use client' // Header requires state
 
 import { useState } from 'react'
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/react'
 
 export default function Header() {
   return (
