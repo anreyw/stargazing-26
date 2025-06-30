c1f263cd5708d6dd6359164ba0ab63a2a0a8ef28
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-29 20:23:49 -0700

    Style the nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
   
   return (
     <header>
-      <nav aria-label="global">
+      <nav aria-label="global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
 	{/*HOME ANCHOR*/}
 	<div>
 	  Logo
