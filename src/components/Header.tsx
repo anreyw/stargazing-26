b64b87c60b7288d4ada906bf11584dcc874fc5c2
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-06-30 15:47:34 -0700

    Copy log in button from template

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -124,6 +124,13 @@ export default function Header() {
             Company
           </a>
         </PopoverGroup>
+
+	{/*LOG IN BUTTON*/}
+	<div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
 	
       </nav>
     </header>
