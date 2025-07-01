1c3ce54f51e52ad4c39211c80f3fa3061daebd13
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-01 13:08:33 -0700

    Add comment labeling background section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
 export default function Hero() {
   return (
     <div className="relative px-6 pt-14 lg:px-8">
+
+      {/*BACKGROUND*/}
       <div
         aria-hidden="true"
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -13,6 +15,7 @@ export default function Hero() {
           className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
         />
       </div>
+      
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -44,6 +47,7 @@ export default function Hero() {
           </div>
         </div>
       </div>
+      
       <div
         aria-hidden="true"
         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
