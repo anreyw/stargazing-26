93d5407857049f01f1b6a9ea4c18ad210f28e447
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-01 11:12:14 -0700

    Remove unnecessary styling from cta div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -114,7 +114,7 @@ export default function Header() {
         </PopoverGroup>
 
 	{/*CALL TO ACTION BUTTON*/}
-	<div className="flex flex-1 items-center justify-end gap-x-6">
+	<div className="flex flex-1 items-center justify-end">
           <a
             href="#"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
