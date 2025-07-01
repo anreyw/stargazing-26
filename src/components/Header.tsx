56e054039ea08577661c8522ae4f5e24a1e2e46b
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-01 11:20:18 -0700

    Change cta button to emerald

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -117,7 +117,7 @@ export default function Header() {
 	<div className="flex flex-1 items-center justify-end">
           <a
             href="#"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
           >
             Register Now
           </a>
