0d17c736867eb3df179c941a8fa9d4ca99edc376
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-03 16:24:02 -0700

    Move hamburger mobile menu button
    According to UX research, most folks expect the hamburger menu icon on
    the top left, so change the order of the nav elements so that
    hamburger icon appears on top left on mobile instead of top right.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,6 +44,19 @@ export default function Header() {
   return (
     <header>
       <nav aria-label="global" className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
+
+	{/*MOBILE MENU HAMBURGER ICON*/}
+	<div className="flex lg:hidden">
+	  <button
+	    type="button"
+	    onClick={() => setMobileMenuOpen(true)}
+	    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+	  >
+	    <span className="sr-only">Open main menu</span>
+	    <Bars3Icon aria-hidden="true" className="size-6" />
+	  </button>
+	</div>
+
 	{/*HOME ANCHOR LOGO*/}
 	<div className="flex lg:flex-1">
 	  <a href="#">
@@ -121,18 +134,6 @@ export default function Header() {
           >
             Register Now
           </a>
-	</div>
-
-	{/*MOBILE MENU HAMBURGER ICON*/}
-	<div className="flex lg:hidden">
-	  <button
-	    type="button"
-	    onClick={() => setMobileMenuOpen(true)}
-	    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-	  >
-	    <span className="sr-only">Open main menu</span>
-	    <Bars3Icon aria-hidden="true" className="size-6" />
-	  </button>
 	</div>
 
       </nav>
