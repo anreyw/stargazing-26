7802b8f26702c78cb033cc1cfcb6decf0dc620df
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-04 10:41:57 -0700

    Move position of mobile menu dialog panel
    Set the position of mobile menu to left instead of right because
    the hamburger icon is now top left.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -141,7 +141,7 @@ export default function Header() {
       {/*MOBILE MENU*/}
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 left-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
 	    <button
               type="button"
