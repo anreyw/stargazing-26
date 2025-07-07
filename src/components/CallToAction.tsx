69a248b397a7e1bf5f9f17336188c3f3e93c1970
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-07 10:53:03 -0700

    Create CallToAction component

---
+++ src/components/CallToAction.tsx
@@ -0,0 +1,12 @@
+export default function CallToAction() {
+  return (
+    <div className="flex flex-1 items-center justify-end">
+      <a
+	href="#"
+	className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+      >
+	 Register Now
+      </a>
+    </div>
+  )
+}
