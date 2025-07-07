9a3ef29f31671a349e6de63ef94b9a511559a7e7
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-07 10:54:37 -0700

    Change Call to Action section so CTA component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -128,14 +128,7 @@ export default function Header() {
         </PopoverGroup>
 
 	{/*CALL TO ACTION BUTTON*/}
-	<div className="flex flex-1 items-center justify-end">
-          <a
-            href="#"
-            className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-          >
-            Register Now
-          </a>
-	</div>
+	<CallToAction />	
 
       </nav>
       
@@ -164,15 +157,7 @@ export default function Header() {
             </a>
 
 	    {/*CALL TO ACTION BUTTON*/}
-	    <div className="flex flex-1 items-center justify-end">
-              <a
-		href="#"
-		className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
-              >
-		 Register Now
-              </a>
-	    </div>
-
+	    <CallToAction />
             
           </div>
           <div className="mt-6 flow-root">
