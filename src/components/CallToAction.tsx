595aba002fa1a3f74723946235ca9a343722b7ad
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-08 12:10:12 -0700

    Change CTA button color
    To match it better with the Hero image.

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -3,7 +3,7 @@ export default function CallToAction() {
     <div className="flex flex-1 items-center justify-end">
       <a
 	href="#"
-	className="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-emerald-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+	className="rounded-md bg-amber-800 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-amber-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-800"
       >
 	 Register Now
       </a>
