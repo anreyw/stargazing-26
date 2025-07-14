e2bdcad2f278fe2657b9b1247f21ee37795ac8ad
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-14 16:01:28 -0700

    Remove Stats from Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from '@/components/Hero'
-import Stats from '@/components/Stats'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
     </>
   )
 }
