fcdd004d30c77f704bed8fedb88d2b12dcdbd154
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-14 15:47:00 -0700

    Add temporary redirect text for Schedule link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -82,36 +82,11 @@ export default function Header() {
               transition
               className="absolute top-full -left-8 z-50 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
             >
-              <div className="p-4">
-                {products.map((item) => (
-                  <div
-                    key={item.name}
-                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50"
-                  >
-                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                      <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
-                    </div>
-                    <div className="flex-auto">
-                      <a href={item.href} className="block font-semibold text-gray-900">
-                        {item.name}
-                        <span className="absolute inset-0" />
-                      </a>
-                      <p className="mt-1 text-gray-600">{item.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                {callsToAction.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
-                  >
-                    <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
-                    {item.name}
-                  </a>
-                ))}
+              <div className="p-12">
+                <p className="text-center pb-4 text-lg"><em>Coming Soon!</em></p>
+		<p>
+		     Check out previous schedules at the main <a className="underline text-blue-700"href="https://www.glaam.org/regional-gathering/">GLAAM Regional Gathering web page</a>.
+		</p>
               </div>
             </PopoverPanel>
           </Popover>
