edb3a3a69228e23eaa2e946ab952e4f5a3883899
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-15 16:54:46 -0700

    Change heading to Registration

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ export default function Registration() {
       <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:max-w-none lg:min-w-full lg:flex-none lg:gap-y-8">
           <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-            <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Our people</h2>
+            <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Registration</h2>
             <p className="mt-6 text-xl/8 text-gray-700">
               Quasi est quaerat. Sit molestiae et. Provident ad dolorem occaecati eos iste. Soluta rerum quidem minus ut
               molestiae velit error quod. Excepturi quidem expedita molestias quas.
