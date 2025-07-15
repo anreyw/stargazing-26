f283689b2e9a4534127c6750809ccf3112e2a962
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-15 16:39:14 -0700

    Fix nav flex styling
    With CTA button conditional now, I am moving nav items to start
    of flex box with justify-items-start.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
   
   return (
     <header>
-      <nav aria-label="global" className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
+      <nav aria-label="global" className="mx-auto flex max-w-7xl items-center justify-items-start gap-x-6 p-6 lg:px-8">
 
 	{/*MOBILE MENU HAMBURGER ICON*/}
 	<div className="flex lg:hidden">
@@ -45,7 +45,7 @@ export default function Header() {
 	</div>
 
 	{/*HOME ANCHOR LOGO*/}
-	<div className="flex lg:flex-1">
+	<div className="flex">
 	  <Link href="/">
 	    <span className="sr-only">Greater Los Angeles Area Mensa</span>
 	    <Image
