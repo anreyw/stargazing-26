bbf30de7c17bc76c8cbc42c7c69cea3e91acf731
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-16 17:00:05 -0700

    Change the anchor to Link

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 export default function CallToAction() {
   return (
     <div className="flex flex-1 items-center">
-      <a
+      <Link
 	href="/register"
 	className="rounded-md bg-amber-800 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-amber-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-800"
       >
 	 Register Now
-      </a>
+      </Link>
     </div>
   )
 }
