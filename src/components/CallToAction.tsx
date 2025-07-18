5c93ec17818026fcea6d2f55b1b29470914d3b8b
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-18 10:16:23 -0700

    Add flex-none to CTA
    - maybe a temporary fix so that the CTA button doesn't look funny
      on smaller screens

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function CallToAction() {
   return (
-    <div className="flex items-center">
+    <div className="flex flex-none items-center">
       <Link
 	href="/register"
 	className="rounded-md bg-amber-800 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-amber-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-800"
