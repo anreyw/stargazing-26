5e833cd706ac8f2eea1867f40be35fc3f3b9007f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-08 16:16:22 -0700

    Improve eyebrow heading
    - Change <div> to <p>
    - add styling
    - change text conten

--- src/components/Keynote.tsx
+++ src/components/Keynote.tsx
@@ -1,7 +1,9 @@
 export default function Keynote() {
   return (
-    <section className="bg-white">
-      <div>Keynote Section</div>
+    <section className="bg-white px-6">
+      <p className="text-base/7 font-semibold text-amber-600">
+        Keynote Speaker
+      </p>
     </section>
   );
 }
