ffd38dc3a624948dd81cd1e5662eaa1339831ce8
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-08 16:08:23 -0700

    Change div to section tag to improve semantics

--- src/components/Keynote.tsx
+++ src/components/Keynote.tsx
@@ -1,7 +1,7 @@
 export default function Keynote() {
   return (
-    <div className="bg-white">
+    <section className="bg-white">
       <div>Keynote Section</div>
-    </div>
+    </section>
   );
 }
