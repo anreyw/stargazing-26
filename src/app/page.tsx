8ca571380d907c95aaaa92334e4ec4ead429c819
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-08 13:22:14 -0700

    Add Keynote component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Keynote from "@/components/Keynote";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Keynote />
     </>
   );
 }
