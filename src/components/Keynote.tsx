f5521d43cfe031fb715c44aadc449b0376275355
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-08 16:20:44 -0700

    Add heading for Keynote

--- src/components/Keynote.tsx
+++ src/components/Keynote.tsx
@@ -4,6 +4,9 @@ export default function Keynote() {
       <p className="text-base/7 font-semibold text-amber-600">
         Keynote Speaker
       </p>
+      <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+        Dave Lopez
+      </h1>
     </section>
   );
 }
