6d5f5d6c70f233f00c7998a91c256bf55c6399f5
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-19 16:24:02 -0700

    Fix small grammar issue

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -7,7 +7,7 @@ export default function SpeakersPage(): JSX.Element {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl sm:text-center">
           <h1 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl dark:text-white">
-            Meet Our Speakers
+            Meet the Speakers
           </h1>
         </div>
         <ul
