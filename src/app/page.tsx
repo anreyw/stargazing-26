7a4c2f0b96e618690c59bf603e5b751e2a52dbd6
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-19 17:54:56 -0700

    Remove Keynote component from Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/Hero";
-import Keynote from "@/components/Keynote";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Keynote />
     </>
   );
 }
