f1380bf5232605e912f7689f61c7269207c47aac
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-19 17:53:39 -0700

    Add Keynote speaker to SpeakersPage
    - Fix add margin below Page title
    - Change title

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -1,15 +1,17 @@
 import { JSX } from "react";
 import speakers from "./speakers";
+import Keynote from "@/components/Keynote";
 
 export default function SpeakersPage(): JSX.Element {
   return (
     <div className="bg-white py-24 sm:py-32 dark:bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl sm:text-center">
+        <div className="mx-auto mb-24 max-w-2xl sm:text-center">
           <h1 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl dark:text-white">
-            Meet the Speakers
+            StaRGazing 2026 Speakers
           </h1>
         </div>
+        <Keynote />
         <ul
           role="list"
           className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none"
