75bb00427d17d707fa624fa4c5ee8c4b9697e7ab
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-19 17:31:19 -0700

    Add two more speakers and remove last template bio

--- src/app/speakers/speakers.ts
+++ src/app/speakers/speakers.ts
@@ -5,22 +5,14 @@ const speakers = [
     bio: "Dave Lehman worked at Jet Propulsion Laboratory (JPL) for 40 as a Technical Manager for the Mars Pathfinder Attitude and Information Management Subsystem.  He was also Project Manager for Deep Space One, the lunar explorer Gravity Recovery and Interior Laboratory (GRAIL) and the Mars Odyssey mission.  Dave will trace humanity's journey to the Red Planet, starting with Mariner 4's pioneering flyby,  the current phase of Mars exploration and advanced missions like Mars Odyssey, Mars Reconnaissance Orbiter, MAVEN and Curiosity  and Perseverance Rovers.  How will these endeavors shape our understanding and presence on Mars? Join us to explore this exciting frontier.",
   },
   {
-    name: "Floyd Miles",
-    imageUrl:
-      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    bio: "Turpis lectus et amet elementum. Mi duis integer sed in vitae consequat. Nam vitae, in felis mi dui tempus. Porta at turpis eu odio. Et, sed duis in blandit bibendum accumsan. Purus viverra facilisi suspendisse quis est.",
+    name: "Jerry Rishe",
+    imageUrl: "jerry-rishe.jpg",
+    bio: "Jerry Rishe worked as an attorney in the private sector capping his career as a trial attorney for Immigration and Naturalization Services.  A true polymath, in retirement, Jerry lectures on a variety of subjects all across the County.   Jerry will talk about the questionable cures and the bold, often dangerous world of self-experimentation. From electric belts and radium cures to lobotomies and miracle tonics, the quest for healing has often pushed the boundaries of science, ethics and common sense.. He will uncover the strange, surprising and sometimes shocking history of how far people have gone in the pursuit of a cure and what it teaches us about the evolving relationship between science, risk and the human body.",
   },
   {
-    name: "Emily Selman",
-    imageUrl:
-      "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    bio: "Aliquet adipiscing lectus praesent cras sed quis lectus egestas erat. Bibendum curabitur eget habitant feugiat nec faucibus eu lorem suscipit. Vitae vitae tempor enim eget lacus nulla leo.",
-  },
-  {
-    name: "Kristin Watson",
-    imageUrl:
-      "https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    bio: "Quis bibendum velit diam tellus sed ut. Faucibus posuere enim, vitae enim eget neque tortor. Metus lectus mattis id id. Tellus ornare etiam id velit ut enim lacinia congue ultrices. Sit morbi vel elit a maecenas mauris elit lectus magna.",
+    name: "Colleen Adair Fliedner",
+    imageUrl: "colleen-adair-fliedner.jpeg",
+    bio: "Award-winning author and historian Colleen Adair Fliedner will delve into the history and legends of Griffith Park and the life of Griffith J. Griffith as described in her latest book, “Fascinating True Tales from Old California.” Griffith tried to murder his wife, went to prison, and afterwards left his property to the City of Los Angeles. His vision was to use the rancho’s remaining land to benefit the “plain man” by transforming it into a beautiful park, building an observatory on Mount Hollywood, and creating a Greek-style outdoor theater. Who was Griffith J. Griffith-a philanthropist, a visionary, a self-serving madman? Colleen will explore the history of the site, and the multifaceted nature of Griffith, whose name endures through the park and brought him fame and immortality.",
   },
 ];
 
